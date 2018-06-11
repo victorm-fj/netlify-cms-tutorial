1387d1785898c5657a8daf046c780752510a0664
Victor Flores <victorm.floresj@gmail.com>
2018-06-11 12:18:50 -0500

    Fix typo in graphql query in blogTemplate

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -18,15 +18,15 @@ const Template = ({ data }) => {
 };
 
 export const pageQuery = graphql`
-  query BlogPostByPath($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html 
-      frontmatter {
-        date(formatString: 'MMMM DD, YYYY')
-        path
-        title
-      }
-    }
-  }
+	query BlogPostByPath($path: String!) {
+		markdownRemark(frontmatter: { path: { eq: $path } }) {
+			html
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				path
+				title
+			}
+		}
+	}
 `;
 export default Template;
