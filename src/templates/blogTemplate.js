d7e10b76844feb60ab52a8af535d7e08533f4ffb
Victor Flores <victorm.floresj@gmail.com>
2018-06-11 12:15:55 -0500

    Fix typo in blogTemplate

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const Template ({ data }) => {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
-  return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-  )
+const Template = ({ data }) => {
+	const { markdownRemark } = data;
+	const { frontmatter, html } = markdownRemark;
+	return (
+		<div className="blog-post-container">
+			<div className="blog-post">
+				<h1>{frontmatter.title}</h1>
+				<h2>{frontmatter.date}</h2>
+				<div
+					className="blog-post-content"
+					dangerouslySetInnerHTML={{ __html: html }}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export const pageQuery = graphql`
@@ -30,4 +30,3 @@ export const pageQuery = graphql`
   }
 `;
 export default Template;
-
